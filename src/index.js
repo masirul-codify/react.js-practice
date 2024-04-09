24b1b0b2c413f41609536d6c07fa5a4cb0ac103f
Masirul Islam <masirulislam80@gmail.com>
2024-04-09 22:12:17 +0600

    006.Learnt Simplify card component and used

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  
+import Card from './Card';
 
-const blogTitle = " Blog App With React  ";
-const blogDesc = " To check your version of Node. js, open Terminal (or another CLI of your choice), and type the command node -v . Press Enter. The returning line will display the Node.Jan 10, 2023 ";
-const date = new Date();
-const currentDay = date.getDay();
-const currentMonth = date.getMonth();
-const currentYear = date.getFullYear();
-const themeBtn = "Read More";
- 
-function Card(){
-  return           <div className='card'>
-  <h1 className='card-title'> {blogTitle} </h1>
-  <h4 className='card-desc'> {blogDesc} </h4>
-  <h6 className='card-footer'> {currentDay + "/" + currentMonth + "/" + currentYear} </h6>
-  <div className='btn-wrapper'>
-    <a href='#' className='theme-btn'>{themeBtn}</a>
-  </div>
-</div> 
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
